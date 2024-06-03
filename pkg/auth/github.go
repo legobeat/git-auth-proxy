2fcffc1215e8adfbc0c846c1f8ff841cce57207b
legobt <6wbvkn0j@anonaddy.me>
2024-06-03 02:16:45 +0000

    feat: forward /repos/ to gh api

--- pkg/auth/github.go
+++ pkg/auth/github.go
@@ -40,15 +40,19 @@ func newGithub(token string) (*github, error) {
 }
 
 func (g *github) getPathRegex(organization, project, repository string) ([]*regexp.Regexp, error) {
-	git, err := regexp.Compile(fmt.Sprintf(`(?i)/%s/%s(/.*)?\b`, organization, repository))
+	git, err := regexp.Compile(fmt.Sprintf(`(?i)/()%s/%s(/.*)?\b`, organization, repository))
 	if err != nil {
 		return nil, err
 	}
-	api, err := regexp.Compile(fmt.Sprintf(`(?i)/api/v3/(.*)/%s/%s/(/.*)?\b`, organization, repository))
+	api, err := regexp.Compile(fmt.Sprintf(`(?i)/api/v[23]/(.*)/%s/%s/(/.*)?\b`, organization, repository))
 	if err != nil {
 		return nil, err
 	}
-	return []*regexp.Regexp{git, api}, nil
+	repos, err := regexp.Compile(fmt.Sprintf(`(?i)/repos/(.*)/%s/%s/(/.*)?\b`, organization, repository))
+	if err != nil {
+		return nil, err
+	}
+	return []*regexp.Regexp{git, api, repos}, nil
 }
 
 func (g *github) getAuthorizationHeader(ctx context.Context, path string) (string, error) {
@@ -68,7 +72,7 @@ func (g *github) getHost(e *Endpoint, path string) string {
 	if e.host != standardGitHub {
 		return e.host
 	}
-	if strings.HasPrefix(path, "/api/v3/") {
+	if strings.HasPrefix(path, "/api/v3/") || strings.HasPrefix(path, "/repos/") {
 		return fmt.Sprintf("api.%s", e.host)
 	}
 	return e.host
